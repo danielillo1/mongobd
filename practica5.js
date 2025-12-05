28b7c0f90519f5fc533d1309d6663029219ab26a
danielillo1 <pacheco.gutierrez.daniel.46@cbtis118.edu.mx>
2025-12-05 11:43:59 -0600

    Add MongoDB queries for various products

---
+++ practica5.js
@@ -0,0 +1,64 @@
+use("bd_py");
+
+
+db.telefono.find({Marca: "Apple"});
+db.telefono.find({Modelo: "Galaxy S25 Ultra"});
+db.telefono.find({Marca: "Google"});
+db.telefono.find({Marca: "Xiaomi"});
+db.telefono.find({Modelo: "Find X8 Pro"});
+db.telefono.find({Marca: "Huawei"});
+db.telefono.find({Marca: "Motorola"});
+db.telefono.find({Marca: "OnePlus"});
+db.telefono.find({Modelo: "Galaxy Z Fold 7"});
+db.telefono.find({Año: 2025});
+
+
+db.videojuegos.find({Plataforma: "Nintendo Switch"});
+db.videojuegos.find({Plataforma: "PlayStation 5"});
+db.videojuegos.find({Plataforma: "Xbox Series X"});
+db.videojuegos.find({Año: 2024});
+db.videojuegos.find({Título: "Elden Ring"});
+db.videojuegos.find({Título: "Hogwarts Legacy"});
+db.videojuegos.find({Año: 2025});
+db.videojuegos.find({Título: "Starfield"});
+db.videojuegos.find({Título: "Gran Turismo 7"});
+
+
+db.audifonos.find({Marca: "Apple"});
+db.audifonos.find({Modelo: "AirPods Max"});
+db.audifonos.find({Marca: "Sony"});
+db.audifonos.find({Modelo: "Sony WH-1000XM5"});
+db.audifonos.find({Marca: "Bose"});
+db.audifonos.find({Marca: "Beats"});
+db.audifonos.find({Modelo: "Beats Studio3 Wireless"});
+db.audifonos.find({Marca: "Jabra"});
+db.audifonos.find({Modelo: "Jabra Elite 85h"});
+db.audifonos.find({Marca: "Sennheiser"});
+
+
+db.tenis.find({Marca: "Nike"});
+db.tenis.find({Modelo: "Nike Air Zoom Pegasus 40"});
+db.tenis.find({Marca: "Adidas"});
+db.tenis.find({Modelo: "Adidas Ultraboost 23"});
+db.tenis.find({Marca: "Puma"});
+db.tenis.find({Modelo: "Puma Speed 1000 v5"});
+db.tenis.find({Marca: "Asics"});
+db.tenis.find({Modelo: "Asics Gel-Kayano 30"});
+db.tenis.find({Marca: "Saucony"});
+db.tenis.find({Modelo: "Saucony Endorphin Speed 3"});
+db.tenis.find({Marca: "New Balance"});
+db.tenis.find({Modelo: "New Balance Fresh Foam 1080v13"});
+
+
+db.bloqueador_solar.find({Marca: "Neutrogena"});
+db.bloqueador_solar.find({Producto: "Neutrogena Ultra Sheer Dry-Touch Sunscreen SPF 100"});
+db.bloqueador_solar.find({Marca: "Coppertone"});
+db.bloqueador_solar.find({Producto: "Coppertone Sport Sunscreen Lotion SPF 50"});
+db.bloqueador_solar.find({Marca: "La Roche-Posay"});
+db.bloqueador_solar.find({Producto: "Anthelios Melt-in Sunscreen Milk SPF 60"});
+db.bloqueador_solar.find({Marca: "Banana Boat"});
+db.bloqueador_solar.find({Producto: "Ultra Sport Sunscreen Lotion SPF 50"});
+db.bloqueador_solar.find({Marca: "Aveeno"});
+db.bloqueador_solar.find({Producto: "Positively Mineral Sensitive Skin Sunscreen SPF 50"});
+db.bloqueador_solar.find({Marca: "EltaMD"});
+db.bloqueador_solar.find({Producto: "UV Clear Broad-Spectrum SPF 46"});
