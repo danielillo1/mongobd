3dcbb0485102454f204c4539bd1539ad07688a9c
danielillo1 <pacheco.gutierrez.daniel.46@cbtis118.edu.mx>
2025-12-05 11:40:23 -0600

    Delete multiple items from database collections
    Remove specified items from various collections in the database.

---
+++ practica3.js
@@ -0,0 +1,16 @@
+use("bd_py");
+
+db.telefono.deleteOne({Modelo: "OnePlus 13"});
+db.telefono.deleteOne({Modelo: "Edge 60 Pro"});
+
+db.videojuegos.deleteOne({Título: "Starfield"});
+db.videojuegos.deleteOne({Título: "Hogwarts Legacy"});
+
+db.audifonos.deleteOne({Modelo: "Google Pixel Buds Pro"});
+db.audifonos.deleteOne({Modelo: "Bose Sport Earbuds"});
+
+db.tenis.deleteOne({Modelo: "Nike Free RN 5.0"});
+db.tenis.deleteOne({Modelo: "Reebok Nano X2"});
+
+db.bloqueador_solar.deleteOne({Producto: "Kids Sunscreen Lotion SPF 50"});
+db.bloqueador_solar.deleteOne({Producto: "CeraVe Hydrating Mineral Sunscreen SPF 30"});
