88ef420e5f9cdaf450111e6e625d31007f12ec44
danielillo1 <pacheco.gutierrez.daniel.46@cbtis118.edu.mx>
2025-12-05 11:33:56 -0600

    Create collections for various products in MongoDB

---
+++ practica1.js
@@ -0,0 +1,8 @@
+
+use("bd_py");
+
+db.createCollection("telefono");
+db.createCollection("videojuegos");
+db.createCollection("audifonos");
+db.createCollection("tenis");
+db.createCollection("bloqueador solar");
